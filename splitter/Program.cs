5532f8de3a61e0ac7562ebb9b48a927a1f44f051
livan.afryanto <livan.afryanto@gmail.com>
2024-01-24 16:17:52 +0700

    Add project files.

---
+++ splitter/Program.cs
@@ -0,0 +1,16 @@
+﻿using jomiunsExtensions;
+
+Dictionary<string, string> aDict = new();
+if (args != null)
+{
+    var aaahaha = args.Select(oo => oo.toKeyValue<string, string>("="));
+    aDict = aaahaha.ToDictionary();
+}
+
+
+var hasil = Console.ReadLine();
+aDict.TryGetValue("delimiter", out var tempDelimiter);
+var charArray = tempDelimiter.otherwise(";").ToCharArray();
+var infos = hasil?.Split(charArray) ?? [];
+var total = string.Join(Environment.NewLine, infos);
+Console.Write(total);
